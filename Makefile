sebasmannem/bdtools

Converting to go

Makefile
@@ -1,0 +1,33 @@
+build:
+	sh ./set_version.sh
+	go mod tidy
+	go build -o ./bin/bdtools ./cmd/bdtools
+
+build_image:
+	docker build . --tag sebasmannem/bdtools
+
+debug:
+	go build -gcflags "all=-N -l" -o ./bin/bdtools ./cmd/bdtools
+	~/go/bin/dlv --headless --listen=:2345 --api-version=2 --accept-multiclient exec ./bin/bdtools -- -c config/bdtools.yaml -d
+
+run:
+	./bin/bdtools -c config/bdtools.yaml -d
+
+fmt:
+	gofmt -w .
+	goimports -w .
+	gci write .
+
+compose:
+	./docker-compose-tests.sh
+
+test: gotest sec lint
+
+sec:
+	gosec ./...
+
+lint:
+	golangci-lint run
+
+gotest:
+	go test -v ./...

internal/main.go
@@ -1,0 +1,21 @@
+package internal
+
+import (
+	"context"
+	"github.com/sebasmannem/bdtools/pkg/quotagroups"
+	"go.uber.org/zap"
+)
+
+var (
+	log *zap.SugaredLogger
+	//ctx context.Context
+)
+
+func InitLogger(logger *zap.SugaredLogger) {
+	log = logger
+	quotagroups.InitLogger(log)
+}
+
+func InitContext(c context.Context) {
+	quotagroups.InitContext(c)
+}

sebasmannem/tplr

Makefile
@@ -1,0 +1,33 @@
+build:
+	sh ./set_version.sh
+	go mod tidy
+	go build -o ./bin/tplr ./cmd/tplr
+
+build_image:
+	docker build . --tag sebasmannem/tplr
+
+debug:
+	go build -gcflags "all=-N -l" -o ./bin/tplr ./cmd/tplr
+	~/go/bin/dlv --headless --listen=:2345 --api-version=2 --accept-multiclient exec ./bin/tplr -- -c config/tplr.yaml -d
+
+run:
+	./bin/tplr -d
+
+fmt:
+	gofmt -w .
+	goimports -w .
+	gci write .
+
+compose:
+	./docker-compose-tests.sh
+
+test: gotest sec lint
+
+sec:
+	gosec ./...
+
+lint:
+	golangci-lint run
+
+gotest:
+	go test -v ./...

internal/main.go
@@ -1,0 +1,21 @@
+package internal
+
+import (
+	"context"
+	"github.com/sebasmannem/tplr/pkg/tekton_handler"
+	"go.uber.org/zap"
+)
+
+var (
+	log *zap.SugaredLogger
+	//ctx context.Context
+)
+
+func InitLogger(logger *zap.SugaredLogger) {
+	log = logger
+	tekton_handler.InitLogger(log)
+}
+
+func InitContext(c context.Context) {
+	tekton_handler.InitContext(c)
+}